57b0e2ad8fa2d3e711a6f7ce6043a55d9504dad9
angie <agpinedam@unal.edu.co>
2025-01-18 15:26:05 +0100

    Solved front problem

---
+++ api/app/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  development: {
+    type: 'development',
+    port: 3000,
+    mongodb: 'mongodb+srv://mines:12345@cluster.mn0xs.mongodb.net/test'
+  },
+  production: {
+    type: 'production',
+    port: 3000,
+    mongodb: 'mongodb+srv://mines:12345@cluster.mn0xs.mongodb.net/test'
+  }
+};
